7238abc9c3ed180c4438f5399d4dd51d2c5cb530
DrAmanJy <dramanjy0770@gmail.com>
2026-04-16 18:29:03 +0530

    feat: integrate v3 authentication routes and establish database connection in server setup

--- apps/api/src/server.js
+++ apps/api/src/server.js
@@ -3,6 +3,9 @@ import cors from 'cors';
 import swaggerUi from 'swagger-ui-express';
 import swaggerSpec from './config/swagger.js';
 
+import v1routes from './v3/auth.routes.js';
+import { connectDb } from './config/db.js';
+
 const server = express();
 
 server.use(cors());
@@ -13,6 +16,9 @@ server.get('/', (_, res) => {
   res.json({ message: 'Server is running' });
 });
 
+server.use('/v3/auth', v1routes);
+
+await connectDb();
 server.listen(3000, (err) => {
   if (err) console.log('Failed to start server', err.message);
   console.log('Server is running on Port: 3000');
