e911890d0befca46e2731c0a67fbf5244f6f49fa
DrAmanJy <dramanjy0770@gmail.com>
2026-04-16 21:18:40 +0530

    feat: add authentication middleware with Basic and Bearer token support

---
+++ apps/api/src/middlewares/auth.middleware.js
@@ -0,0 +1,62 @@
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+import User from '../models/user.js';
+
+export const isAuthV1 = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith('Basic '))
+    return res
+      .status(401)
+      .json({ status: false, message: 'Not authorized, no credentials provided' });
+
+  const base = authHeader.split(' ')[1];
+
+  if (!base)
+    return res
+      .status(401)
+      .json({ status: false, message: 'Not authorized, no credentials provided' });
+
+  const decoded = Buffer.from(base, 'base64').toString('utf-8');
+  const [email, password] = decoded.split(':');
+
+  const user = await User.findOne({ email }).select('+password');
+  if (!user)
+    return res
+      .status(401)
+      .json({ status: false, message: 'Not authorized, invalid credentials provided' });
+
+  const isPassValid = await bcrypt.compare(password, user.password);
+
+  if (!isPassValid)
+    return res
+      .status(401)
+      .json({ status: false, message: 'Not authorized, invalid credentials provided' });
+
+  req.user = user;
+  next();
+};
+
+export const isAuthV3 = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith('Bearer '))
+    return res.status(401).json({ status: 'fail', message: 'Not authorized, no token provided' });
+
+  const accessToken = authHeader.split(' ')[1];
+  if (!accessToken)
+    return res.status(401).json({ status: 'fail', message: 'Invalid token format' });
+
+  let decoded;
+  try {
+    decoded = jwt.verify(accessToken, process.env.JWT_ACCESS_SECRET);
+  } catch {
+    return res.status(401).json({ message: 'Invalid or expired access token' });
+  }
+
+  const user = await User.findById(decoded.id);
+  if (!user)
+    return res.status(401).json({ message: 'The user belonging to this token no longer exists' });
+
+  req.user = user;
+  next();
+};
