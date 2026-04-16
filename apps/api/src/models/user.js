cf577675cd74534ba7713d91544c6c79c9be52d5
DrAmanJy <dramanjy0770@gmail.com>
2026-04-16 18:27:03 +0530

    feat: create user model with validation and password hashing

---
+++ apps/api/src/models/user.js
@@ -0,0 +1,45 @@
+import { Schema, model } from 'mongoose';
+import bcrypt from 'bcrypt';
+
+const userSchema = new Schema(
+  {
+    name: { type: String, required: true, trim: true },
+    email: {
+      type: String,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      required: true,
+      validate: {
+        validator: function (v) {
+          return /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(v);
+        },
+        message: (props) => `${props.value} is not valid email`,
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      min: [6, 'Password must be 6 char long'],
+      select: false,
+    },
+  },
+  {
+    timestamps: true,
+    toJSON: {
+      virtuals: true,
+      transform: function (doc, ret) {
+        delete ret._id;
+        delete ret.password;
+      },
+    },
+  },
+);
+
+userSchema.pre('save', async function () {
+  if (!this.isModified('password')) return;
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+const User = model('User', userSchema);
+export default User;
