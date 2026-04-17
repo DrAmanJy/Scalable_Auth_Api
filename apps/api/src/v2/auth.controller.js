7ef8220b200fbdad88a735f7ecc30ad7cebe0bf3
DrAmanJy <dramanjy0770@gmail.com>
2026-04-17 21:59:12 +0530

    feat: implement user login functionality in auth controller with password validation and token generation

--- apps/api/src/v2/auth.controller.js
+++ apps/api/src/v2/auth.controller.js
@@ -1,5 +1,6 @@
 import User from '../models/user.js';
 import { createTokenV1, hashToken } from '../utils/token.js';
+import bcrypt from 'bcrypt';
 
 const cookieOptions = {
   httpOnly: true,
@@ -32,4 +33,34 @@ export const register = async (req, res) => {
     .status(201)
     .cookie('refreshToken', refreshToken, cookieOptions)
     .json({ status: 'success', message: 'User register successfully', user, accessToken });
+};
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password)
+    return res.status(400).json({ status: 'fail', message: 'All fields are required' });
+
+  const user = await User.findOne({ email }).select('+password');
+
+  if (!user) return res.status(400).json({ status: 'fail', message: 'Invalid email or password' });
+
+  const isValidPass = bcrypt.compare(password, user.password);
+
+  if (!isValidPass)
+    return res.status(400).json({ status: 'fail', message: 'Invalid email or password' });
+
+  const accessToken = createTokenV1({ id: user.id });
+  const refreshToken = createTokenV1({ id: user.id }, 'refresh');
+
+  const hashRefreshToken = await hashToken(refreshToken);
+
+  user.refreshToken = hashRefreshToken;
+
+  await user.save();
+
+  res
+    .status(200)
+    .cookie('refreshToken', refreshToken, cookieOptions)
+    .json({ status: 'success', message: 'User successfully login', user, accessToken });
 };
