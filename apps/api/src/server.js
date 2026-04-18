8d2d92b98c745e4ce544614af3baeab1b7aea129
DrAmanJy <dramanjy0770@gmail.com>
2026-04-18 18:28:43 +0530

    feat: integrate cookie-parser and add v4 authentication routes to the server

--- apps/api/src/server.js
+++ apps/api/src/server.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
 import swaggerUi from 'swagger-ui-express';
 import swaggerSpec from './config/swagger.js';
 
 import v1routes from './v1/auth.routes.js';
 
 import v3routes from './v3/auth.routes.js';
+import v4routes from './v4/auth.routes.js';
 
 import { connectDb } from './config/db.js';
 import errorHandler from './middlewares/error.middleware.js';
@@ -14,6 +17,7 @@ const server = express();
 
 server.use(cors());
 server.use(express.json({ limit: '2kb' }));
+server.use(cookieParser());
 server.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 server.get('/', (_, res) => {
@@ -22,6 +26,7 @@ server.get('/', (_, res) => {
 
 server.use('/v1/auth', v1routes);
 server.use('/v3/auth', v3routes);
+server.use('/v4/auth', v4routes);
 server.use(errorHandler);
 
 await connectDb();
