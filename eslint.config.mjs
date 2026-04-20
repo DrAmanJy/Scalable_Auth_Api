49d67f24c6741352a2063af77c45648bb0dfe5b6
DrAmanJy <dramanjy0770@gmail.com>
2026-04-20 22:28:52 +0530

    refactor: enhance no-unused-vars rule in ESLint configuration to ignore variable patterns for improved code quality

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,7 +12,10 @@ export default defineConfig([
     extends: ['js/recommended'],
     languageOptions: { globals: globals.node },
     rules: {
-      'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      'no-unused-vars': [
+        'warn',
+        { argsIgnorePattern: '^_', varsIgnorePattern: '^_', caughtErrorsIgnorePattern: '^_' },
+      ],
       'no-console': 'off',
 
       'no-undef': 'error',
