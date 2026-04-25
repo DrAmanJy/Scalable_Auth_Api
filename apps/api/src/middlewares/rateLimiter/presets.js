d38105dfeadf5667c70ed9eb7ed0166574abd316
DrAmanJy <dramanjy0770@gmail.com>
2026-04-26 00:08:08 +0530

    refactor: update rate limiter presets to use default strategy and token bucket store for improved request management

--- apps/api/src/middlewares/rateLimiter/presets.js
+++ apps/api/src/middlewares/rateLimiter/presets.js
@@ -1,9 +1,13 @@
 import { createRateLimiter } from './createRateLimiter.js';
 import { ipKeyGenerator, ipUserComboKeyGenerator, userKeyGenerator } from './keyGenerators.js';
+
+// Stores
 import FixedWindowStore from './stores/fixedWindowLimiter.js';
 import SlidingWindowLogStore from './stores/slidingWindowLogLimiter.js';
-import { fixedWindowStrategy } from './strategies/fixedWindow.js';
-import { slidingWindowStrategy } from './strategies/slidingWindowLog.js';
+import TokenBucketStore from './stores/tokenBucketStore.js';
+
+// Strategy
+import { defaultStrategy } from './strategies/defaultStrategy.js';
 
 /**
  * 1. TESTING LIMITER
@@ -11,10 +15,9 @@ import { slidingWindowStrategy } from './strategies/slidingWindowLog.js';
  * Limit: 3 requests per 10 seconds.
  */
 export const testLimiter = createRateLimiter({
-  strategy: slidingWindowStrategy,
-  store: new SlidingWindowLogStore(10_000),
-  limit: 3,
-  windowMs: 10_000,
+  strategy: defaultStrategy,
+  store: new TokenBucketStore(10, 2, 10_000),
+  limit: 10,
   keyGenerator: ipUserComboKeyGenerator,
 });
 
@@ -24,10 +27,9 @@ export const testLimiter = createRateLimiter({
  * Limit: 5 attempts per 15 minutes.
  */
 export const authLimiter = createRateLimiter({
-  strategy: slidingWindowStrategy,
+  strategy: defaultStrategy,
   store: new SlidingWindowLogStore(15 * 60_000),
   limit: 5,
-  windowMs: 15 * 60_000,
   keyGenerator: userKeyGenerator,
 });
 
@@ -37,10 +39,9 @@ export const authLimiter = createRateLimiter({
  * Limit: 60 requests per minute.
  */
 export const publicApiLimiter = createRateLimiter({
-  strategy: fixedWindowStrategy,
+  strategy: defaultStrategy,
   store: new FixedWindowStore(60_000),
   limit: 60,
-  windowMs: 60_000,
   keyGenerator: ipKeyGenerator,
 });
 
@@ -50,9 +51,8 @@ export const publicApiLimiter = createRateLimiter({
  * Limit: 1000 requests per minute.
  */
 export const assetLimiter = createRateLimiter({
-  strategy: fixedWindowStrategy,
+  strategy: defaultStrategy,
   store: new FixedWindowStore(60_000),
   limit: 1000,
-  windowMs: 60_000,
   keyGenerator: ipKeyGenerator,
 });
