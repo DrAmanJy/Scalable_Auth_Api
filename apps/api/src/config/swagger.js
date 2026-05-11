ef1ace14184883f1c77647815de88878192a10e6
DrAmanJy <dramanjy0770@gmail.com>
2026-05-11 15:56:23 +0530

    docs(swagger): enhance API documentation configuration

--- apps/api/src/config/swagger.js
+++ apps/api/src/config/swagger.js
@@ -13,8 +13,31 @@ const options = {
         url: 'http://localhost:3000',
       },
     ],
+    components: {
+      securitySchemes: {
+        basicAuth: {
+          type: 'http',
+          scheme: 'basic',
+        },
+        cookieAuth: {
+          type: 'apiKey',
+          in: 'cookie',
+          name: 'sessionId',
+        },
+        bearerAuth: {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT',
+        },
+        deviceIdAuth: {
+          type: 'apiKey',
+          in: 'cookie',
+          name: 'deviceId',
+        },
+      },
+    },
   },
-  apis: ['./src/v1/routes/*.js', './src/v2/routes/*.js'],
+  apis: ['./src/v1/*.routes.js', './src/v2/*.routes.js', './src/v3/*.routes.js', './src/v4/*.routes.js', './src/v5/*.routes.js'],
 };
 
 const swaggerSpec = swaggerJSDoc(options);
